81123b95df67bc73ec05d4cda324267e70f02b0b
Katy DeCorah <decorah@mapbox.com>
2021-02-12 14:44:13 -0500

    Sort Items by required then optional (#492)
    Co-Authored-By: Dom Brassey <dom.brassey@mapbox.com>
    
    Co-authored-by: Dom Brassey <dom.brassey@mapbox.com>

--- docs/components/style-spec/items.js
+++ docs/components/style-spec/items.js
@@ -5,41 +5,56 @@ import Item from './item.js';
 
 export default class Items extends React.Component {
     render() {
+        const orderEntries = Object.keys(this.props.entry).reduce(
+            (obj, item) => {
+                if (this.props.entry[item].required)
+                    obj.required[item] = this.props.entry[item];
+                else obj.optional[item] = this.props.entry[item];
+                return obj;
+            },
+            {
+                required: {},
+                optional: {}
+            }
+        );
+        const sortEntries = [
+            ...entries(orderEntries.required).sort(), // sort required entries
+            ...entries(orderEntries.optional).sort() // sort optional entries
+        ];
+
         return (
             <React.Fragment>
-                {entries(this.props.entry)
-                    .sort()
-                    .map(([name, prop], i) => {
-                        const entry = this.props.entry[name];
-                        const section = entry.section || this.props.section;
-                        const kind = entry.kind || this.props.kind;
-                        const headingLevel =
-                            entry.headingLevel || this.props.headingLevel;
-                        // if section begins with the name of a source type, do not display an item for * or type
-                        if (
-                            [
-                                'vector',
-                                'raster',
-                                'raster-dem',
-                                'geojson',
-                                'image',
-                                'video'
-                            ].indexOf(this.props.section) > -1 &&
-                            (name === '*' || name === 'type')
-                        )
-                            return;
+                {sortEntries.map(([name, prop], i) => {
+                    const entry = this.props.entry[name];
+                    const section = entry.section || this.props.section;
+                    const kind = entry.kind || this.props.kind;
+                    const headingLevel =
+                        entry.headingLevel || this.props.headingLevel;
+                    // if section begins with the name of a source type, do not display an item for * or type
+                    if (
+                        [
+                            'vector',
+                            'raster',
+                            'raster-dem',
+                            'geojson',
+                            'image',
+                            'video'
+                        ].indexOf(this.props.section) > -1 &&
+                        (name === '*' || name === 'type')
+                    )
+                        return;
 
-                        return (
-                            <Item
-                                key={i}
-                                id={`${section ? `${section}-` : ''}${name}`}
-                                name={name}
-                                {...prop}
-                                kind={kind}
-                                headingLevel={headingLevel}
-                            />
-                        );
-                    })}
+                    return (
+                        <Item
+                            key={i}
+                            id={`${section ? `${section}-` : ''}${name}`}
+                            name={name}
+                            {...prop}
+                            kind={kind}
+                            headingLevel={headingLevel}
+                        />
+                    );
+                })}
             </React.Fragment>
         );
     }
